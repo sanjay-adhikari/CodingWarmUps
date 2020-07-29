6c07b6fc1dfcc62297b15aaf27217952deeed065
sanjay-adhikari <47584174+sanjay-adhikari@users.noreply.github.com>
2020-07-28 20:44:40 -0500

    Create OddOccurrencesInArrayCodility.cs
    A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
    
    For example, in array A such that:
    
      A[0] = 9  A[1] = 3  A[2] = 9
      A[3] = 3  A[4] = 9  A[5] = 7
      A[6] = 9
    the elements at indexes 0 and 2 have value 9,
    the elements at indexes 1 and 3 have value 3,
    the elements at indexes 4 and 6 have value 9,
    the element at index 5 has value 7 and is unpaired.
    Write a function:
    
    class Solution { public int solution(int[] A); }
    
    that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
    
    For example, given array A such that:
    
      A[0] = 9  A[1] = 3  A[2] = 9
      A[3] = 3  A[4] = 9  A[5] = 7
      A[6] = 9
    the function should return 7, as explained in the example above.
    
    Write an efficient algorithm for the following assumptions:
    
    N is an odd integer within the range [1..1,000,000];
    each element of array A is an integer within the range [1..1,000,000,000];
    all but one of the values in A occur an even number of times.

---
+++ OddOccurrencesInArrayCodility.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrencesInArray
+{
+    class Program
+    {
+        static int GetOddOccurrencesInArray(int[] num)
+        {
+            Dictionary<int,int> dict = new Dictionary<int, int>();
+            foreach (int n in num)
+            {
+                if (dict.ContainsKey(n))
+                {
+                    dict[n]++;
+                }
+                else
+                {
+                    dict.Add(n, 1);
+                }
+            }
+            var oddVal = dict.FirstOrDefault(x => x.Value == 1).Key; //Get Vlaue from Key using Lambda expression
+            return oddVal;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+            int oddValue = GetOddOccurrencesInArray(nums);
+
+            Display("Input", nums);
+            Console.WriteLine("The odd occurence is {0}", oddValue);
+
+        }
+        static void Display(string s, int[] num)
+        {
+            Console.WriteLine("{0}:", s);
+            foreach (var n in num)
+                Console.Write("{0} \t", n);
+            Console.WriteLine("\n");
+        }
+    }
+}
