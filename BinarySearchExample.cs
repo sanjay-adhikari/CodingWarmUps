1118c1569748e234e74f14efa791db33ce95f13d
Sanjay Adhikari <47584174+sanjay-adhikari@users.noreply.github.com>
2020-09-09 10:19:36 -0500

    Update BinarySearchExample.cs

--- BinarySearchExample.cs
+++ BinarySearchExample.cs
@@ -11,19 +11,44 @@ namespace BinarySearchExample
             Console.WriteLine("{0} is found at index {1}", searchValue, BinarySearch(numbers, searchValue));
             Console.ReadKey();
         }
-        public static int BinarySearch(int[] nums, int target)
+        public static int BinarySearch(int[] nums, int target) //Iterative approach
         {
-            //return Array.IndexOf(nums,target); This is linear
-            
             int midIndex, leftIndex = 0, rightIndex=nums.Length-1;
             while (leftIndex <= rightIndex)
             {
                 midIndex = leftIndex + (rightIndex - leftIndex) / 2;
-                if (nums[midIndex] == target) return midIndex;
-                if (target < nums[midIndex]) rightIndex = midIndex - 1;
-                else leftIndex = midIndex + 1;
+                if (nums[midIndex] == target) 
+                    return midIndex; //This is what we are looking for
+                
+                if (target < nums[midIndex]) 
+                    rightIndex = midIndex - 1;
+                else 
+                    leftIndex = midIndex + 1;
             }
             return -1;
+        }
+        
+        public static int BinarySearchRecursive(int[] nums, int low, int high, int key) //Using Recursive Approach
+        {
+            if(low == high)
+            {
+                if(nums[low] == key)
+                    return low;
+                else
+                    return -1;
+            }
+            else
+            {
+                int mid = (low + high) /2;
+                
+                if(key == nums[mid])
+                    return mid;
+                
+                if(key < nums[mid])
+                    return BinarySearchRecursive(nums, low, mid -1, key);
+                else
+                    return BinarySearchRecursive(nums, mid + 1, high, key);
+            }
         }
     }
 }
